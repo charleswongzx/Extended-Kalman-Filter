4973039ea70192f314f332cdf9bde80d1421c2d0
Charles Wong <charleswongzx@gmail.com>
2018-05-20 18:29:08 +0800

    Updating with latest

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -11,39 +11,45 @@
 
 class FusionEKF {
 public:
-  /**
-  * Constructor.
-  */
-  FusionEKF();
+    /**
+    * Constructor.
+    */
+    FusionEKF();
 
-  /**
-  * Destructor.
-  */
-  virtual ~FusionEKF();
+    /**
+    * Destructor.
+    */
+    virtual ~FusionEKF();
 
-  /**
-  * Run the whole flow of the Kalman Filter from here.
-  */
-  void ProcessMeasurement(const MeasurementPackage &measurement_pack);
+    /**
+    * Run the whole flow of the Kalman Filter from here.
+    */
+    void ProcessMeasurement(const MeasurementPackage &measurement_pack);
 
-  /**
-  * Kalman Filter update and prediction math lives in here.
-  */
-  KalmanFilter ekf_;
+    /**
+    * Kalman Filter update and prediction math lives in here.
+    */
+    KalmanFilter ekf_;
 
 private:
-  // check whether the tracking toolbox was initialized or not (first measurement)
-  bool is_initialized_;
+    // check whether the tracking toolbox was initialized or not (first measurement)
+    bool is_initialized_;
 
-  // previous timestamp
-  long long previous_timestamp_;
+    // noise parameters
+    float noise_px_;
+    float noise_py_;
+    float noise_vx_;
+    float noise_vy_;
 
-  // tool object used to compute Jacobian and RMSE
-  Tools tools;
-  Eigen::MatrixXd R_laser_;
-  Eigen::MatrixXd R_radar_;
-  Eigen::MatrixXd H_laser_;
-  Eigen::MatrixXd Hj_;
+    // previous timestamp
+    long long previous_timestamp_;
+
+    // tool object used to compute Jacobian and RMSE
+    Tools tools;
+    Eigen::MatrixXd R_laser_;
+    Eigen::MatrixXd R_radar_;
+    Eigen::MatrixXd H_laser_;
+    Eigen::MatrixXd Hj_;
 };
 
 #endif /* FusionEKF_H_ */
--- src/tools.cpp
+++ src/tools.cpp
@@ -11,15 +11,40 @@ Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+    /**
+    TODO:
+      * Calculate the RMSE here.
+    */
+    VectorXd rmse(4);
+    rmse << 0,0,0,0;
+    int n = estimations.size();
+    for (int i=0; i<n; i++) {
+        VectorXd residual_error = estimations[i] - ground_truth[i];
+        residual_error = (residual_error.array()*residual_error.array());
+        rmse += residual_error;
+    }
+    rmse = rmse / n;
+    rmse = rmse.array().sqrt();
+    return rmse;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
-  /**
-  TODO:
-    * Calculate a Jacobian here.
-  */
+    /**
+    TODO:
+      * Calculate a Jacobian here.
+    */
+    MatrixXd Hj(3,4);
+    float px = x_state(0);
+    float py = x_state(1);
+    float vx = x_state(2);
+    float vy = x_state(3);
+
+    float c1 = px*px + py*py;
+    if (fabs(c1) < 0.00001) c1 = 0.00001;
+    float c2 = sqrt(c1);
+    float c3 = c1*c2;
+    Hj << (px/c2), (py/c2), 0, 0,
+            -(py/c1), (px/c1), 0, 0,
+            (py*(vx*py - vy*px)/c3), (px*(vy*px - vx*py)/c3), (px/c2), (py/c2);
+    return Hj;
 }
